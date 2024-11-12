88afe52abe793f0507a372e589b52e603ccae623
webidare <webidevolution@gmail.com>
2024-11-13 02:59:32 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
+import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={`${inter.className} bg-pink-50`}>
         {children}
+        <Toaster />
       </body>
     </html>
   )
